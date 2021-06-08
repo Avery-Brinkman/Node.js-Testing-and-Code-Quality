c54814dd274b8d375c9fae6e68740dd103601042
Avery Brinkman <brinkmae@mail.uc.edu>
2021-06-08 17:08:15 -0400

    Formatting

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Nadia\'s Garden' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Nadia's Garden" });
 });
 
-router.delete('/', function(req, res, next) {
+router.delete("/", function (req, res, next) {
   res.end(500);
 });
 
--- routes/reservations.js
+++ routes/reservations.js
@@ -1,30 +1,31 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var debug = require('debug')('nadia:route:reservations')
-const reservations = require('../lib/reservations');
-const Reservation = require('../lib/schema/reservation');
+var debug = require("debug")("nadia:route:reservations");
+const reservations = require("../lib/reservations");
+const Reservation = require("../lib/schema/reservation");
 
-router.get('/', function(req, res, next) {
-  res.render('reservations');
+router.get("/", function (req, res, next) {
+  res.render("reservations");
 });
 
-router.post('/', function(req, res, next) {
+router.post("/", function (req, res, next) {
   const reservation = new Reservation(req.body);
-  reservations.create(reservation)
-    .then(reservationId => res.render('reservations', {
-      success: true,
-      reservationId
-    }))
-    .catch(err => {
+  reservations
+    .create(reservation)
+    .then((reservationId) =>
+      res.render("reservations", {
+        success: true,
+        reservationId,
+      })
+    )
+    .catch((err) => {
       debug(err.message, req.body);
-      res.status(400).render('reservations', {
-        errors: [
-          err.message
-        ],
+      res.status(400).render("reservations", {
+        errors: [err.message],
         success: false,
-        submission: req.body
+        submission: req.body,
       });
-    })
+    });
 });
 
 module.exports = router;
