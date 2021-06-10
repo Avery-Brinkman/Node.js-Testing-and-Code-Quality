e4bbd9782667ab5586eed404b2c5887c81dd8aa2
Avery Brinkman <brinkmae@mail.uc.edu>
2021-06-10 12:56:33 -0400

    Controlling responses with stubs

--- test/unit/lib/reservations.js
+++ test/unit/lib/reservations.js
@@ -1,9 +1,21 @@
+const proxyquire = require("proxyquire");
+const sinon = require("sinon");
 const chai = require("chai");
 const should = chai.should();
 const Reservation = require("../../../lib/schema/reservation");
-const reservations = require("../../../lib/reservations");
 
 describe("Reservations Library", function () {
+  const debugStub = function () {
+    return sinon.stub();
+  };
+  let reservations;
+
+  before(function () {
+    reservations = proxyquire("../../../lib/reservations", {
+      debug: debugStub,
+    });
+  });
+
   context("Validate", function () {
     it("should pass a valid reservation with no optional fields", function () {
       const reservation = new Reservation({
